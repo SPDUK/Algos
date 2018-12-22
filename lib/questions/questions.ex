53cf9f4823f7aa68c984f36ba978dd22aa632fa1
spdevuk <spdevuk@gmail.com>
2018-12-22 02:01:35 +0000

    Adding integer reversal question

---
+++ lib/questions/questions.ex
@@ -0,0 +1,24 @@
+defmodule Questions do
+  @moduledoc """
+    Some answers to some typical interview/programming questions.
+  """
+
+  @doc """
+  In elixir you can't do simply use to_integer on a string such as "003-", it will error. Instead we have to use Integer.parse, which returns a tuple, we pattern match out the reversed integer and don't care about the non-integer part of the tuple.
+  """
+  def reverse_int(n) do
+    {reversed, _} =
+      n
+      |> Integer.to_string()
+      |> String.split("")
+      |> Enum.reverse()
+      |> Enum.join("")
+      |> Integer.parse()
+
+    if n < 0 do
+      reversed * -1
+    else
+      reversed
+    end
+  end
+end

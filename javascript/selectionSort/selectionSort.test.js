2fa3f718e3b56bd348022f6aa27c9960016377ef
spdevuk <spdevuk@gmail.com>
2018-12-31 18:23:13 +0000

    fixing formatting

--- javascript/selectionSort/selectionSort.test.js
+++ javascript/selectionSort/selectionSort.test.js
@@ -1,17 +1,17 @@
-const selectionSort = require("./selectionSort.js");
+const selectionSort = require('./selectionSort.js');
 
-test("Should not unsort the numbers", () => {
+test('Should not unsort the numbers', () => {
   expect(selectionSort([1, 2, 3, 4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6]);
 });
 
-test("Should sort the numbers", () => {
+test('Should sort the numbers', () => {
   expect(selectionSort([1, 6, 3, 5, 2, 4])).toEqual([1, 2, 3, 4, 5, 6]);
 });
 
-test("Should sort the numbers", () => {
+test('Should sort the numbers', () => {
   expect(selectionSort([6, 5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5, 6]);
 });
 
-test("Should not infinite loop", () => {
+test('Should not infinite loop', () => {
   expect(selectionSort([])).toEqual([]);
 });

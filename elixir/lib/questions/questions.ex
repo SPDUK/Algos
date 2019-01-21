d8f4659e75462f62a2c8b914f444454090c649c3
SPDUK <spdevuk@gmail.com>
2019-01-21 10:27:46 +0000

    adding some anonymous function stuff

--- elixir/lib/questions/questions.ex
+++ elixir/lib/questions/questions.ex
@@ -49,4 +49,21 @@ defmodule Questions do
       IO.puts(str)
     end
   end
+
+  ##### Some things for Programming Elixir book #####
+
+  # Anonymous functions
+
+  list_concat = fn [a, b], [c, d] -> [a, b, c, d] end
+  IO.inspect(list_concat.([1, 2], [99, 100]))
+  # [1, 2, 99, 100]
+
+  # using capturing
+  sum = &(&1 + &2 + &3)
+  IO.inspect(sum.(99, 8, 77))
+  # 184
+
+  pair_tuple_to_list = fn {a, b} -> [a, b] end
+  IO.inspect(pair_tuple_to_list.({1234, 5678}))
+  # [1234,5678]
 end
